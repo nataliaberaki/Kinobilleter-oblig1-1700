e8f696129c327e412282af1946cd63c67c7f5618
nataliaberaki <naber1286@oslomet.no>
2024-02-18 18:00:54 +0100

    endra navn på filmer, lagt til tabell, ut() funskjon, slettBilletter funksjon

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -3,13 +3,13 @@ let billetter = [];
 
 function visBilletter(){
     let kjøptFilm = document.getElementById("filmer").value;
-    let antallBilletter = document.getElementById("antall").value;
+    let antallBilletter = document.getElementById("antallBilletter").value;
     let gittnavn = document.getElementById("fornavn").value;
     let slektsnavn = document.getElementById("etternavn").value;
     let tlf = document.getElementById("telefon").value;
     let mail = document.getElementById("epost").value;
 
-    let billett1 = {
+    const billett1 = {
         filmer: kjøptFilm,
         antall: antallBilletter,
         fornavn: gittnavn,
@@ -19,10 +19,39 @@ function visBilletter(){
     };
 
     billetter.push(billett1);
+    ut();
+
+    document.getElementById("filmer").value = "";
+    document.getElementById("antallBilletter").value = "";
+    document.getElementById("fornavn").value = "";
+    document.getElementById("etternavn").value = "";
+    document.getElementById("telefon").value = "";
+    document.getElementById("epost").value = "";
 
 }
 
+function ut(){
+    let skrivUt = "<table><tr><th>Film</th><th>Antall</th><th>Fornavn</th><th>Etternavn</th><th>Telefon</th><th>E-post</th></tr>";
+    for(let i = 0; i < billetter.length; i++){
+        skrivUt += "<tr>";
+        skrivUt += "<td>" + billetter[i].filmer + "</td>";
+        skrivUt += "<td>" + billetter[i].antall + "</td>";
+        skrivUt += "<td>" + billetter[i].fornavn + "</td>";
+        skrivUt += "<td>" + billetter[i].etternavn + "</td>";
+        skrivUt += "<td>" + billetter[i].telefon + "</td>";
+        skrivUt += "<td>" + billetter[i].epost + "</td>";
+        skrivUt += "</tr>";
+    }
 
-let ut = "<t<ble><tr>" +
-    "<th>Film</th><th>Antall</th><th>Fornavn</th><th>Etternavn</th><th>Telefon</th><th>E-post</th>" +
-    "</tr>";
+    skrivUt += "</table>";
+    document.getElementById("kjøptBilletter").innerHTML = skrivUt;
+
+
+}
+
+function slettBilletter(){
+    billetter = [];
+    console.log(billetter);
+    ut();
+}
+
